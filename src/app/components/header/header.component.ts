dbb3f525e93481c2b2ae92d793fb3b3959860728
Kevin Vargas <kevin.vargas@laureate.net>
2019-05-03 02:45:58 -0600

    Nav bar implementation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,11 +9,15 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   permissions: any;
+  openMenu: boolean = false;
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {}
   checkPermission(key: string){
     return this.authService.evaluatePermissions(key);
+  }
+  toogleMenu(){
+    this.openMenu = !this.openMenu;
   }
   logout(){
     this.authService.logout();
